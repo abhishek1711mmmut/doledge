219b0fa91d7b621745918034aba6ddc43e57604e
abhishek17111mmmut <abhishek1711mmmut@gmail.com>
2024-02-05 17:03:33 +0530

    login issue solved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,49 +21,48 @@ import VisualEntry from "./components/PaymentScreen/ResumeWriting/VisualResume/V
 import VisualMiddle from "./components/PaymentScreen/ResumeWriting/VisualResume/VisualMiddle";
 import VisualSenior from "./components/PaymentScreen/ResumeWriting/VisualResume/VisualSenior";
 
-import InternationalTextEntry from './components/PaymentScreen/International/InternationalTextResume/InternationalTextEntry';
-import InternationalTextMiddle from './components/PaymentScreen/International/InternationalTextResume/InternationalTextMiddle';
-import InternationalTextSenior from './components/PaymentScreen/International/InternationalTextResume/InternationalTextSenior';
-import InternationalVisualEntry from './components/PaymentScreen/International/InternationalVisualResume/InternationalVisualEntry';
-import InternationalVisualMiddle from './components/PaymentScreen/International/InternationalVisualResume/InternationalVisualMiddle';
-import InternationalVisualSenior from './components/PaymentScreen/International/InternationalVisualResume/InternationalVisualSenior';
-import ZapInternational from './components/PaymentScreen/International/ZapInternational';
+import InternationalTextEntry from "./components/PaymentScreen/International/InternationalTextResume/InternationalTextEntry";
+import InternationalTextMiddle from "./components/PaymentScreen/International/InternationalTextResume/InternationalTextMiddle";
+import InternationalTextSenior from "./components/PaymentScreen/International/InternationalTextResume/InternationalTextSenior";
+import InternationalVisualEntry from "./components/PaymentScreen/International/InternationalVisualResume/InternationalVisualEntry";
+import InternationalVisualMiddle from "./components/PaymentScreen/International/InternationalVisualResume/InternationalVisualMiddle";
+import InternationalVisualSenior from "./components/PaymentScreen/International/InternationalVisualResume/InternationalVisualSenior";
+import ZapInternational from "./components/PaymentScreen/International/ZapInternational";
 
-import ZapResume from './components/PaymentScreen/CareerGrowth/ZapResume';
-import HighlightResume from './components/PaymentScreen/CareerGrowth/HighlightResume';
-import JobSearchAssistant3 from './components/PaymentScreen/CareerGrowth/JobSearchAssistant3';
-import InterviewPreparation from './components/PaymentScreen/CareerGrowth/InterviewPreparation';
-import TopManagementProfile from './components/PaymentScreen/CareerGrowth/TopManagementProfile';
-import ProfileUpdate from './components/PaymentScreen/CareerGrowth/ProfileUpdate';
-import PersonalPortfolio from './components/PaymentScreen/CareerGrowth/PersonalPortfolio';
-import CoverLetter from './components/PaymentScreen/CareerGrowth/CoverLetter';
-import EmployeeVerification from './components/PaymentScreen/ScreeningServices/EmployeeVerification';
-import ComboResume from './components/PaymentScreen/ScreeningServices/ComboResume';
-import ResumeQualityScore from './components/PaymentScreen/ResumeWriting/ResumeQualityScore/ResumeQualityScore';
+import ZapResume from "./components/PaymentScreen/CareerGrowth/ZapResume";
+import HighlightResume from "./components/PaymentScreen/CareerGrowth/HighlightResume";
+import JobSearchAssistant3 from "./components/PaymentScreen/CareerGrowth/JobSearchAssistant3";
+import InterviewPreparation from "./components/PaymentScreen/CareerGrowth/InterviewPreparation";
+import TopManagementProfile from "./components/PaymentScreen/CareerGrowth/TopManagementProfile";
+import ProfileUpdate from "./components/PaymentScreen/CareerGrowth/ProfileUpdate";
+import PersonalPortfolio from "./components/PaymentScreen/CareerGrowth/PersonalPortfolio";
+import CoverLetter from "./components/PaymentScreen/CareerGrowth/CoverLetter";
+import EmployeeVerification from "./components/PaymentScreen/ScreeningServices/EmployeeVerification";
+import ComboResume from "./components/PaymentScreen/ScreeningServices/ComboResume";
+import ResumeQualityScore from "./components/PaymentScreen/ResumeWriting/ResumeQualityScore/ResumeQualityScore";
 import ResumeCritique from "./components/PaymentScreen/ResumeWriting/ResumeCritique";
 
 // import COntactus from './components/Contactus'
-import SocialProfiler from './components/PaymentScreen/International/SocialProfiler';
-
+import SocialProfiler from "./components/PaymentScreen/International/SocialProfiler";
 
 // import TextEntryResumeref from './components/PaymentScreen/ResumeWriting/TextResume/TextResumeref';
 
-import contextAuth from './ContextAPI/ContextAuth';
-import { useEffect, useState } from 'react';
-import TextExecutive from './components/PaymentScreen/ResumeWriting/TextResume/TextExecutive';
-import VisualExecutive from './components/PaymentScreen/ResumeWriting/VisualResume/VisualExecutive';
-import GoogleNewPassword from './components/GoogleNewPassword';
-import Spinner from './UI/Spinner/Spinner';
-import DataLoading from './UI/DataLoading/DataLoading';
-import Modal from './UI/Modal/Modal';
-import axios from 'axios';
-import { responsiveFontSizes } from '@mui/material';
-import InternationalTextExecutive from './components/PaymentScreen/International/InternationalTextResume/InternationalTextExecutive';
-import InternationalVisualExecutive from './components/PaymentScreen/International/InternationalVisualResume/InternationalVisualExecuive';
-import Dashboard from './components/Dashboard';
-import DocumentVerification from './components/PaymentScreen/ScreeningServices/DocumentVerification';
-import Contactus from './components/Contactus';
-import JobAlertOnMailAndSms from './components/PaymentScreen/CareerGrowth/JobAlertOnMailAndSms';
+import contextAuth from "./ContextAPI/ContextAuth";
+import { useEffect, useState } from "react";
+import TextExecutive from "./components/PaymentScreen/ResumeWriting/TextResume/TextExecutive";
+import VisualExecutive from "./components/PaymentScreen/ResumeWriting/VisualResume/VisualExecutive";
+import GoogleNewPassword from "./components/GoogleNewPassword";
+import Spinner from "./UI/Spinner/Spinner";
+import DataLoading from "./UI/DataLoading/DataLoading";
+import Modal from "./UI/Modal/Modal";
+import axios from "axios";
+import { responsiveFontSizes } from "@mui/material";
+import InternationalTextExecutive from "./components/PaymentScreen/International/InternationalTextResume/InternationalTextExecutive";
+import InternationalVisualExecutive from "./components/PaymentScreen/International/InternationalVisualResume/InternationalVisualExecuive";
+import Dashboard from "./components/Dashboard";
+import DocumentVerification from "./components/PaymentScreen/ScreeningServices/DocumentVerification";
+import Contactus from "./components/Contactus";
+import JobAlertOnMailAndSms from "./components/PaymentScreen/CareerGrowth/JobAlertOnMailAndSms";
 import CombosHighlights from "./components/PaymentScreen/CareerGrowth/CombosHighlights";
 
 function App() {
@@ -94,21 +93,14 @@ function App() {
 
   const logout = () => {
     loadingHandler(true);
-    axios
-      .get(`${process.env.REACT_APP_SERVER_PRO_URL}/logout`, {
-        withCredentials: true,
-      })
-      .then(() => {
-        localStorage.removeItem("userID");
-        localStorage.removeItem("email");
-        localStorage.removeItem("userName");
-        localStorage.removeItem("picture");
-        localStorage.removeItem("token");
-        setUser({});
-        setToken("");
-        loadingHandler(false);
-      })
-      .catch((err) => console.log(err));
+    localStorage.removeItem("userID");
+    localStorage.removeItem("email");
+    localStorage.removeItem("userName");
+    localStorage.removeItem("picture");
+    localStorage.removeItem("token");
+    setUser({});
+    setToken("");
+    loadingHandler(false);
   };
 
   const errorHandler = (error) => {
@@ -120,69 +112,147 @@ function App() {
   };
 
   return (
-        <contextAuth.Provider value={{user, token, login, logout, error, errorHandler, loading, setLoading, loadingHandler}}>
-          <BrowserRouter>
-            <div className="App">
-                <Navbar/>
-                {error && <Modal error={error.message} type={error.type}/>}
-                {loading && <Spinner/>}
-                <Routes>
-                {/* <Route path="/Contact-us" element={<Contactus/>} /> */}
-                  {/* <Route path="/redirect" element={<Redirect/>} /> */}
-                  {/* <Route path="/Text-Resume-Entry-Levelref" element={<TextEntryResumeref/>} /> */}
-                  <Route path="/Text-Resume-Entry-Level" element={<TextEntry/>} />
-                  <Route path="/Text-Resume-Middle-Level" element={<TextMiddle/>} />
-                  <Route path="/Text-Resume-Senior-Level" element = {<TextSenior/>} />
-                  <Route path="/Text-Resume-Executive-Level" element = {<TextExecutive/>} />
-                  <Route path="/Visual-Resume-Entry-Level" element={<VisualEntry/>} />
-                  <Route path="/Visual-Resume-Middle-Level" element={<VisualMiddle/>} />
-                  <Route path="/Visual-Resume-Senior-Level" element = {<VisualSenior/>} />
-                  <Route path="/Visual-Resume-Executive-Level" element = {<VisualExecutive/>} />
-                  <Route path='/resume-quality-score' element={<ResumeQualityScore/>}/>
-                  <Route path="/International-Resume-Entry-Level" element={<InternationalTextEntry/>} />
-                  <Route path="/International-Resume-Middle-Level" element={<InternationalTextMiddle/>} />
-                  <Route path="/International-Resume-Senior-Level" element = {<InternationalTextSenior/>} />
-                  <Route path="/International-Resume-Executive-Level" element = {<InternationalTextExecutive/>} />
-                  <Route path="/Visual-Resume-International-Entry-Level" element={<InternationalVisualEntry/>} />
-                  <Route path="/Visual-Resume-International-Middle-Level" element={<InternationalVisualMiddle/>} />
-                  <Route path="/Visual-Resume-International-Senior-Level" element = {<InternationalVisualSenior/>} />
-                  <Route path="/Visual-Resume-International-Executive-Level" element = {<InternationalVisualExecutive/>} />
-                  
-                  <Route path="/Zap-Your-Resume-International" element={<ZapInternational/>} />
-                  <Route path="Zap-Your-Resume" element={<ZapResume/>} />
-                  <Route path="Highlight-Your-Resume" element={<HighlightResume/>} />
-                  <Route path="/Job-Search-Assistant" element={<JobSearchAssistant3/>} />
-                  <Route path="/Interview-Preparation" element={<InterviewPreparation/>} />
-                  <Route path="Job-Alert-On-Mail-And-SMS" element={<JobAlertOnMailAndSms/>} />
-                  <Route path="/Top-Management-Profile" element={<TopManagementProfile/>} />
-                  <Route path="/Profile-Update" element={<ProfileUpdate/>} />
-                  <Route path="/Personal-Portfolio" element={<PersonalPortfolio/>} />
-                  <Route path="/Cover-Letter" element={<CoverLetter/>} />
-                  <Route path="/Employee-Background-Verification" element={<EmployeeVerification/>} />
-                  <Route path="/Resume-Critique" element={<ResumeCritique/>} />
-                  <Route path="/Combo-Resume-Pack" element={<ComboResume/>} />
-                  <Route path='/Document-Verification' element={<DocumentVerification/>}/>
-                  <Route path="/Social-Profiler" element={<SocialProfiler/>} />
-                  <Route path="/dashboard" element={<Dashboard/>}/>
-                  <Route path="/Combos-Highlights" element={<CombosHighlights />} />
-                  
-                  {!token && (
-                    <>
-                      <Route path="/register" element={<Register/>} />
-                      <Route path="/login" element={<Login/>} />
-                    </>
-                  )}
-                  
-                  <Route path="/auth/loadLoginData" element={<DataLoading/>}/>
-                  <Route path="/auth/googlePassword" element={<GoogleNewPassword/>}/>
-                    <Route path="/" element={<MAINheader/>} />
-                  {/* <Route path='*' element={<MAINheader/>}/> */}
-                </Routes>
-                <Blog/>
-                <Footer/>
-            </div>
-          </BrowserRouter>
-        </contextAuth.Provider>
+    <contextAuth.Provider
+      value={{
+        user,
+        token,
+        login,
+        logout,
+        error,
+        errorHandler,
+        loading,
+        setLoading,
+        loadingHandler,
+      }}
+    >
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          {error && <Modal error={error.message} type={error.type} />}
+          {loading && <Spinner />}
+          <Routes>
+            {/* <Route path="/Contact-us" element={<Contactus/>} /> */}
+            {/* <Route path="/redirect" element={<Redirect/>} /> */}
+            {/* <Route path="/Text-Resume-Entry-Levelref" element={<TextEntryResumeref/>} /> */}
+            <Route path="/Text-Resume-Entry-Level" element={<TextEntry />} />
+            <Route path="/Text-Resume-Middle-Level" element={<TextMiddle />} />
+            <Route path="/Text-Resume-Senior-Level" element={<TextSenior />} />
+            <Route
+              path="/Text-Resume-Executive-Level"
+              element={<TextExecutive />}
+            />
+            <Route
+              path="/Visual-Resume-Entry-Level"
+              element={<VisualEntry />}
+            />
+            <Route
+              path="/Visual-Resume-Middle-Level"
+              element={<VisualMiddle />}
+            />
+            <Route
+              path="/Visual-Resume-Senior-Level"
+              element={<VisualSenior />}
+            />
+            <Route
+              path="/Visual-Resume-Executive-Level"
+              element={<VisualExecutive />}
+            />
+            <Route
+              path="/resume-quality-score"
+              element={<ResumeQualityScore />}
+            />
+            <Route
+              path="/International-Resume-Entry-Level"
+              element={<InternationalTextEntry />}
+            />
+            <Route
+              path="/International-Resume-Middle-Level"
+              element={<InternationalTextMiddle />}
+            />
+            <Route
+              path="/International-Resume-Senior-Level"
+              element={<InternationalTextSenior />}
+            />
+            <Route
+              path="/International-Resume-Executive-Level"
+              element={<InternationalTextExecutive />}
+            />
+            <Route
+              path="/Visual-Resume-International-Entry-Level"
+              element={<InternationalVisualEntry />}
+            />
+            <Route
+              path="/Visual-Resume-International-Middle-Level"
+              element={<InternationalVisualMiddle />}
+            />
+            <Route
+              path="/Visual-Resume-International-Senior-Level"
+              element={<InternationalVisualSenior />}
+            />
+            <Route
+              path="/Visual-Resume-International-Executive-Level"
+              element={<InternationalVisualExecutive />}
+            />
+
+            <Route
+              path="/Zap-Your-Resume-International"
+              element={<ZapInternational />}
+            />
+            <Route path="Zap-Your-Resume" element={<ZapResume />} />
+            <Route path="Highlight-Your-Resume" element={<HighlightResume />} />
+            <Route
+              path="/Job-Search-Assistant"
+              element={<JobSearchAssistant3 />}
+            />
+            <Route
+              path="/Interview-Preparation"
+              element={<InterviewPreparation />}
+            />
+            <Route
+              path="Job-Alert-On-Mail-And-SMS"
+              element={<JobAlertOnMailAndSms />}
+            />
+            <Route
+              path="/Top-Management-Profile"
+              element={<TopManagementProfile />}
+            />
+            <Route path="/Profile-Update" element={<ProfileUpdate />} />
+            <Route path="/Personal-Portfolio" element={<PersonalPortfolio />} />
+            <Route path="/Cover-Letter" element={<CoverLetter />} />
+            <Route
+              path="/Employee-Background-Verification"
+              element={<EmployeeVerification />}
+            />
+            <Route path="/Resume-Critique" element={<ResumeCritique />} />
+            <Route path="/Combo-Resume-Pack" element={<ComboResume />} />
+            <Route
+              path="/Document-Verification"
+              element={<DocumentVerification />}
+            />
+            <Route path="/Social-Profiler" element={<SocialProfiler />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/Combos-Highlights" element={<CombosHighlights />} />
+
+            {!token && (
+              <>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </>
+            )}
+
+            <Route path="/auth/loadLoginData" element={<DataLoading />} />
+            <Route
+              path="/auth/googlePassword"
+              element={<GoogleNewPassword />}
+            />
+            <Route path="/" element={<MAINheader />} />
+            {/* <Route path='*' element={<MAINheader/>}/> */}
+          </Routes>
+          <Blog />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </contextAuth.Provider>
   );
 }
 
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import '../css/Register.css';
+import "../css/Register.css";
 
 import InputElement from "../UI/InputElement/InputElement";
 import axios from "axios";
@@ -7,128 +7,161 @@ import contextAuth from "../ContextAPI/ContextAuth";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const Auth = useContext(contextAuth);
-    let [overAllValid, setOverAllValid] = useState(false)
-    let [inputs, setInputs] = useState({
-        email: {
-            value: '',
-            isValid: true,
-        },
-        password: {
-            value: '',
-            isValid: true,
-        },
-    })
+  const navigate = useNavigate();
+  const Auth = useContext(contextAuth);
+  let [overAllValid, setOverAllValid] = useState(false);
+  let [inputs, setInputs] = useState({
+    email: {
+      value: "",
+      isValid: true,
+    },
+    password: {
+      value: "",
+      isValid: true,
+    },
+  });
 
-    const changeHandler = (event, type) => {
-        let value = event.target.value;
-        let updatedInputs = {...inputs};
-        
-        if(!value || value.length == 0)
-        updatedInputs[type].isValid = false;
-        else
-        updatedInputs[type].isValid = true; 
+  const changeHandler = (event, type) => {
+    let value = event.target.value;
+    let updatedInputs = { ...inputs };
 
-        updatedInputs[type].value = value;
-        setInputs(updatedInputs);
-        overAllValidity()
+    if(type == "email") {
+        if(!value.includes("@gmail.com")){
+            updatedInputs[type].isValid = false;
+        } else updatedInputs[type].isValid = true;
+    } else {
+        if(value.length <= 8 || value.length >= 20){
+            updatedInputs[type].isValid = false;
+        } else updatedInputs[type].isValid = true;
     }
 
-    const overAllValidity = () => {
-        let {email, password} = inputs;
-        if(email.value && password.value)
-        setOverAllValid(true)
-        else 
-        setOverAllValid(false)
-    }
+    updatedInputs[type].value = value;
+    setInputs(updatedInputs);
+    overAllValidity();
+  };
 
-    const googleLoginHandler = () => {
-        event.preventDefault()
-        window.open(`${process.env.REACT_APP_SERVER_PRO_URL}/signin/google`, '_self');
-      }
+  const overAllValidity = () => {
+    let { email, password } = inputs;
+    if (email.isValid && password.isValid) setOverAllValid(true);
+    else setOverAllValid(false);
+  };
 
-    const submitFormHanadler = (event) => {
-        event.preventDefault();
-        Auth.loadingHandler(true);
-        const {email, password} = inputs;
-        
-        let data = {}
-        data.email = email.value;
-        data.password = password.value;
-    
-        axios.post(`${process.env.REACT_APP_SERVER_PRO_URL}/api/auth/login`, data)
-        .then(response => {
-            let data = response.data;
-            if(data.status == 'success'){
-                Auth.login(data.user, data.token)
-                Auth.loadingHandler(false);
-                navigate('/')
-            }
-            else {
-                Auth.loadingHandler(false)
-                Auth.errorHandler({message: data.error, type: data.type})
-                // console.log(response.data)
-            }
-        })
-        .catch(err => console.log(err))
-      }
+  const googleLoginHandler = () => {
+    event.preventDefault();
+    window.open(
+      `${process.env.REACT_APP_SERVER_PRO_URL}/signin/google`,
+      "_self"
+    );
+  };
 
-    return (
-        <div className='Register w-full flex flex-col justify-center items-center'>
-            <form onSubmit={submitFormHanadler} className="reg-form w-[85%] flex flex-col py-4 px-4 m-5 
+  const submitFormHanadler = (event) => {
+    event.preventDefault();
+    Auth.loadingHandler(true);
+    const { email, password } = inputs;
+
+    let data = {};
+    data.email = email.value;
+    data.password = password.value;
+
+    axios
+      .post(`${process.env.REACT_APP_SERVER_PRO_URL}/api/auth/login`, data)
+      .then((response) => {
+        let data = response.data;
+        if (data.status == "success") {
+          Auth.login(data.user, data.token);
+          Auth.loadingHandler(false);
+          navigate("/");
+        } else {
+          Auth.loadingHandler(false);
+          Auth.errorHandler({ message: data.error, type: data.type });
+          // console.log(response.data)
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="Register w-full flex flex-col justify-center items-center">
+      <form
+        onSubmit={submitFormHanadler}
+        className="reg-form w-[85%] flex flex-col py-4 px-4 m-5 
             sm:w-[80%]
             md:w-[70%]
-            lg:w-[55%]">
-                {/* Form Inputs */}
-                <h4 className="mb-5 mt-1 text-left">Find a job & grow your career</h4>
-                <div className="w-full flex flex-col
-                sm:flex-row sm:justify-center sm:items-center">
-                    <div className="w-[100%] mr-3
-                    sm:w-[70%]">
-                        <InputElement
-                        field={'email'}
-                        type={'email'}
-                        label={'Email ID'}
-                        placeholder={'Tell us your Email ID'}
-                        error={'Email ID is required'}
-                        value={inputs.email.value}
-                        valid={inputs.email.isValid}
-                        onChange={changeHandler}/>
+            lg:w-[55%]"
+      >
+        {/* Form Inputs */}
+        <h4 className="mb-5 mt-1 text-left">Find a job & grow your career</h4>
+        <div
+          className="w-full flex flex-col
+                sm:flex-row sm:justify-center sm:items-center"
+        >
+          <div
+            className="w-[100%] mr-3
+                    sm:w-[70%]"
+          >
+            <InputElement
+              field={"email"}
+              type={"email"}
+              label={"Email ID"}
+              placeholder={"Tell us your Email ID"}
+              error={"Please enter a valid gmail."}
+              value={inputs.email.value}
+              valid={inputs.email.isValid}
+              onChange={changeHandler}
+            />
 
-                        <InputElement
-                        field={'password'}
-                        type={'password'}
-                        label={'Password'}
-                        placeholder={'Create a password for your account'}
-                        error={'Password is required'}
-                        value={inputs.password.value}
-                        valid={inputs.password.isValid}
-                        onChange={changeHandler}/>
-                    </div>
-                    {/* Google Register */}
-                    <div className="google-container flex justify-center text-center items-center w-[270px] p-2 m-2
-                    sm:flex-col sm:w-[30%] sm:m-0 sm:p-0 sm:mb-3 sm:h-[160px]">
-                        <h6 className="font-bold mt-1 mr-2 sm:mt-0 sm:mr-0 sm:mb-2">Continue With</h6>
-                        <button className="google flex flex-row" onClick={googleLoginHandler}>
-                            <div><img className="w-[20px] [h-20px] mr-2 mt-0.5" src={require("../images/google-48.png")}/></div>
-                            <div>Goolge</div>
-                        </button>
-                    </div>
-                </div>
-
-                {/* Submit Button */}
-                <div className="submitWrapper flex flex-col text-left mt-2">
-                    <p className="gray13px">By clicking Sign in, you agree to the Terms and Conditions & Privacy Policy of Doledge.com</p>
-                    <button
-                    type="submit" 
-                    className="submit text-left mt-2"
-                    disabled={overAllValid ? false : true}
-                    style={{backgroundColor: !overAllValid && '#ccc'}}>Sign in</button>
-                </div>
-            </form>
+            <InputElement
+              field={"password"}
+              type={"password"}
+              label={"Password"}
+              placeholder={"Create a password for your account"}
+              error={"Password must be between 8-20 characters long."}
+              value={inputs.password.value}
+              valid={inputs.password.isValid}
+              onChange={changeHandler}
+            />
+          </div>
+          {/* Google Register */}
+          <div
+            className="google-container flex justify-center text-center items-center w-[270px] p-2 m-2
+                    sm:flex-col sm:w-[30%] sm:m-0 sm:p-0 sm:mb-3 sm:h-[160px]"
+          >
+            <h6 className="font-bold mt-1 mr-2 sm:mt-0 sm:mr-0 sm:mb-2">
+              Continue With
+            </h6>
+            <button
+              className="google flex flex-row"
+              onClick={googleLoginHandler}
+            >
+              <div>
+                <img
+                  className="w-[20px] [h-20px] mr-2 mt-0.5"
+                  src={require("../images/google-48.png")}
+                />
+              </div>
+              <div>Goolge</div>
+            </button>
+          </div>
         </div>
-    )
-}
+
+        {/* Submit Button */}
+        <div className="submitWrapper flex flex-col text-left mt-2">
+          <p className="gray13px">
+            By clicking Sign in, you agree to the Terms and Conditions & Privacy
+            Policy of Doledge.com
+          </p>
+          <button
+            type="submit"
+            className="submit text-left mt-2"
+            disabled={overAllValid ? false : true}
+            style={{ backgroundColor: !overAllValid && "#ccc" }}
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
